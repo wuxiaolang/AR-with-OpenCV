11ed08b0b54be9e2f0964665c95031701388feb2
wuxiaolang <wutongyuxuan@163.com>
2019-01-08 23:36:04 +0800

    update markerAr

--- markerAr/v0_1_video.cpp
+++ markerAr/v0_1_video.cpp
@@ -1,41 +1,46 @@
-////#include <opencv2/core/core.hpp>        //openCV核心库
-//#include <opencv2/imgproc/imgproc.hpp>  //openCV图像处理库
-//#include <opencv2/highgui/highgui.hpp>  //用户界面库，播视频要用
-//#include <opencv2/calib3d/calib3d.hpp>  //相机校准，姿态估计用
-//
-//using namespace std;
-//using namespace cv;
-//
-////Scalar是一个四元组,这里对应bgra
-////Scalar blue(255, 0, 0);
-////Scalar green(0, 255, 0);
-////Scalar red(0, 0, 255);
-//
-////所使用的marker的边大小，可自定义
-//const int marker_width = 200;
-//
-//int main(int argc, char** argv) {
-//
-//    //Mat即矩阵，常常用Mat存储图像
-//    Mat image;
-//
-//    //捕捉视频用的对象，参数改为0会使用摄像头输入。
-//    VideoCapture cap("/home/wuxiaolang/视频/demo.mp4");
-//
-//    if(!cap.isOpened())
-//        return -1;
-//
-//    //cap.grab()即抓取下一帧
-//    while (cap.grab())
-//    {
-//        //解码抓取的帧并放入image中
-//        cap.retrieve(image);
-//
-//        //新建窗口并显示捕捉到的图片，第一个参数为窗口名。
-//        cv::imshow("image", image);
-//
-//        //延迟100ms 自己机子上做就设置1
-//        cv::waitKey(1);
-//    }
-//    return 0;
-//}
+/**
+ * @file 	v0_1_video.cpp，对应与可执行文件v0_1_video
+ * @brief 	此程序用于测试用Opencv读取本地视频中的每一帧图像.
+ * @author	wuyanminmax@gmail.com
+ * @date 	2018.01.29
+ */
+
+#include <opencv2/core/core.hpp>        //openCV核心库
+#include <opencv2/imgproc/imgproc.hpp>  //openCV图像处理库
+#include <opencv2/highgui/highgui.hpp>  //用户界面库，播视频要用
+#include <opencv2/calib3d/calib3d.hpp>  //相机校准，姿态估计用
+#include <iostream>
+
+using namespace cv;
+
+//所使用的marker的边大小，可自定义.
+const int marker_width = 200;
+
+int main(int argc, char** argv) 
+{
+   std::cout << "【v0_1_video】 is running ……" << std::endl;
+
+   //定义图像帧.
+   Mat image;
+
+   //捕捉视频用的对象，参数改为0或1（电脑相机的编号）会使用摄像头输入。
+   VideoCapture cap("/home/wuxiaolang/视频/demo.mp4");
+
+   // 判断是否读取成功.
+   if(!cap.isOpened())
+       return -1;
+
+   //cap.grab()即抓取下一帧
+   while (cap.grab())
+   {
+       //解码抓取的帧并放入image中
+       cap.retrieve(image);
+
+       //新建窗口并显示捕捉到的图片，第一个参数为窗口名。
+       cv::imshow("image", image);
+
+       //延迟100ms 自己机子上做就设置1
+       cv::waitKey(1);
+   }
+   return 0;
+}
--- markerAr/v0_2_cam.cpp
+++ markerAr/v0_2_cam.cpp
@@ -1,33 +1,39 @@
-///*
-// * 此程序用于读取相机图像
-// *
-// */
-//#include <opencv2/highgui/highgui.hpp>
-//#include <opencv2/imgproc/imgproc.hpp>
-//#include <opencv2/calib3d/calib3d.hpp>
-//#include <iostream>
-//
-//using namespace cv;
-//
-//int main()
-//{
-//    VideoCapture capture(0);
-//    Mat image;
-//
-//    if(!capture.isOpened())
-//    {
-//        std::cout << "读取相机失败" << std::endl;
-//        return -1;
-//    }
-//
-//    while(capture.grab())
-//    {
-//        capture >> image;
-//        resize(image, image, Size(960, 720));
-//        std::cout << "图像大小：" << image.cols << "*" << image.rows << std::endl;
-//        imshow("相机", image);
-//        waitKey(1);
-//    }
-//
-//    return 0;
-//}
+/**
+ * @file 	v0_2_cam.cpp  对应与可执行文件 v0_2_cam
+ * @brief 	此程序用于测试用Opencv读取相机图像.
+ * @author	wuyanminmax@gmail.com
+ * @date 	2018.01.29
+ */
+
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <iostream>
+
+using namespace cv;
+
+int main()
+{
+   std::cout << "【v0_2_cam】 is running ……" << std::endl;
+
+   // 读取并定义相机图像.
+   VideoCapture capture(0);
+   Mat image;
+
+   if(!capture.isOpened())
+   {
+       std::cout << "读取相机失败" << std::endl;
+       return -1;
+   }
+
+   while(capture.grab())
+   {
+       capture >> image;
+       resize(image, image, Size(960, 720));
+       std::cout << "图像大小：" << image.cols << "*" << image.rows << std::endl;
+       imshow("相机", image);
+       waitKey(1);
+   }
+
+   return 0;
+}
